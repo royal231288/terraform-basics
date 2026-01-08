4eb6f4bd26dc6b727012395a8a98c88658cbcb3b
Md Sarowar Alam <sarowar@vml.com>
2026-01-08 20:03:06 +0600

    Your First Step into Terraform & Infrastructure as Code

--- first-ec2-example/first_ec2.tf
+++ first-ec2-example/first_ec2.tf
@@ -1,10 +1,20 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "PUT-YOUR-ACCESS-KEY-HERE"
-  secret_key = "PUT-YOUR-SECRET-KEY-HERE"
+  access_key = "ID"
+  secret_key = "ID"
 }
 
 resource "aws_instance" "myec2" {
-  ami           = "ami-00c39f71452c08778"
-  instance_type = "t2.micro"
+  ami                    = "ami-00f46ccd1cbfb363e"
+  instance_type          = "t3.medium"
+  key_name               = "ostad-batch-08"
+  iam_instance_profile   = "SSM"
+  vpc_security_group_ids = ["sg-07cc5ad28f68e8e55"]
+  subnet_id              = "subnet-03672544009e3c40e"
+
+  associate_public_ip_address = true
+  tags = {
+    Name = "MyFirstEC2Instance"
+  }
+
 }
