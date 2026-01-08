4abb4370539402fd3f452fb1c4c6e17c25f2be61
Md Sarowar Alam <sarowar@vml.com>
2026-01-08 20:48:50 +0600

    Your First Step into Terraform & Infrastructure as Code_03

--- ec2-project/modules/ec2/main.tf
+++ ec2-project/modules/ec2/main.tf
@@ -2,17 +2,9 @@ resource "aws_instance" "this" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
-
   vpc_security_group_ids = [var.security_group_id]
-
   key_name = var.key_name != "" ? var.key_name : null
-
-  tags = merge(
-    {
-      Name = var.instance_name
-    },
-    var.tags
-  )
+  associate_public_ip_address = true
 
   root_block_device {
     volume_size           = 8
@@ -26,8 +18,16 @@ resource "aws_instance" "this" {
       var.tags
     )
   }
-
   lifecycle {
     create_before_destroy = false
   }
+
+  tags = merge(
+    {
+      Name = var.instance_name
+    },
+    var.tags
+  )
+
+
 }
